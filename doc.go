90f1975057a40a66c85f7e2d18ad2dfcce3bfa59
Bas van Beek <bas@tobin.nl>
2023-08-21 20:36:35 +0200

    improve package doc comment

--- doc.go
+++ doc.go
@@ -13,5 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package opentelemetry provides ...
+// Package opentelemetry provides a telemetry Metric interface implementation
+// based on OpenTelemetry.
 package opentelemetry
